4467db170442ea84079d500dbc322e220767c5f7
fredalbert37 <fredalbert37@outlook.com>
2022-12-23 19:18:27 -0500

    mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProductModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/test_backend'),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,47 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+class Category {
+  name: string;
+  slug: string;
+}
+
+class Brand {
+  name: string;
+  slug: string;
+}
+
+@Schema()
+export class Product extends Document {
+  @Prop({
+    required: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    required: true,
+    index: true,
+  })
+  category: Category;
+
+  @Prop({
+    required: true,
+    index: true,
+  })
+  brand: Brand;
+
+  @Prop({
+    required: true,
+    unique: true,
+    index: true,
+  })
+  slug: string;
+
+  @Prop({
+    required: true,
+  })
+  status: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
